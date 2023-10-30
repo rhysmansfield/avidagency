6f4e160e31241d536ce319b928e1b9de5e25c0ec
Rhys Mansfield <r.mansfield2002@gmail.com>
2023-10-30 11:34:41 +0000

    Add GTM to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "@app/components/header/header";
 import { poppins } from "@app/fonts/poppins";
 import "@app/styles/reset.scss";
 import "@app/styles/global.scss";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Avid Media | Amplify, Visualise, Innovate, Deliver",
@@ -35,6 +36,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PSPRMMW8');
+        `}
+        </Script>
+      </head>
       <ReCaptchaProvider reCaptchaKey={process.env.RECAPTCHA_PUBLIC_KEY}>
         <body className={poppins.className}>
           <Header />
