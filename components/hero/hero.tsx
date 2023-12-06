3b1858e31dee27691deba5ce208bcae2395cf580
Rhys Mansfield <r.mansfield2002@gmail.com>
2023-12-06 13:59:54 +0000

    Improve hero code quality

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -5,25 +5,23 @@ import { HeroProps } from "./hero.type";
 
 import styles from "./hero.module.scss";
 
-export const Hero = ({ title, text }: HeroProps) => {
-  return (
-    <section className={styles.root}>
-      <div className="wrapper">
-        <h1 className={styles.title}>
-          <Animation animation="slide-up">{title[0]}</Animation>
-          <Animation
-            animation="slide-up"
-            delay={0.3}
-            className={styles.highlight}
-          >
-            {title[1]}
-          </Animation>
-        </h1>
-
-        <Animation animation="fade-in" delay={1} className={styles.text}>
-          {text}
+export const Hero = ({ title, text }: HeroProps) => (
+  <section className={styles.root}>
+    <div className="wrapper">
+      <h1 className={styles.title}>
+        <Animation animation="slide-up">{title[0]}</Animation>
+        <Animation
+          animation="slide-up"
+          delay={0.3}
+          className={styles.highlight}
+        >
+          {title[1]}
         </Animation>
-      </div>
-    </section>
-  );
-};
+      </h1>
+
+      <Animation animation="fade-in" delay={1} className={styles.text}>
+        {text}
+      </Animation>
+    </div>
+  </section>
+);
