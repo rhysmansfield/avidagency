32e7943e49cc1bbdd78f31821a1d7ddf74334729
Rhys Mansfield <r.mansfield2002@gmail.com>
2023-12-06 12:30:07 +0000

    Add types to hero

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import { Hero } from "@/components/hero/hero";
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero
+        title={["Avid", "Media"]}
+        text={
+          <>
+            We work with a range of companies to transform their digital
+            identity, through <span>best-in-class</span> digital marketing and
+            web development.
+          </>
+        }
+      />
     </>
   );
 }
--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -4,22 +4,21 @@ import { TextSlideUp } from "@/components/animation/text-slide-up/text-slide-up"
 import { TextFadeIn } from "../animation/text-fade-in/text-fade-in";
 
 import styles from "./hero.module.scss";
+import { HeroProps } from "./hero.type";
 
-export const Hero = () => {
+export const Hero = ({ title, text }: HeroProps) => {
   return (
     <section className={styles.root}>
       <div className="wrapper">
         <h1 className={styles.title}>
-          <TextSlideUp>Avid</TextSlideUp>
+          <TextSlideUp>{title[0]}</TextSlideUp>
           <TextSlideUp delay={0.3} className={styles.highlight}>
-            Media
+            {title[1]}
           </TextSlideUp>
         </h1>
 
         <TextFadeIn delay={1} className={styles.text}>
-          We work with a range of companies to transform their digital identity,
-          through <span>best-in-class</span> digital marketing and web
-          development.
+          {text}
         </TextFadeIn>
       </div>
     </section>
