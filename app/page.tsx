c6a835635a71de02c85d98cca34b5c6856015026
Rhys Mansfield <r.mansfield2002@gmail.com>
2023-12-11 17:20:54 +0000

    Move hero props to json & fix css margin issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,11 @@
 import { Hero } from "@/components/hero/hero";
 
+import { homepageHeroProps } from "@/json/homepage";
+
 export default function Home() {
   return (
     <>
-      <Hero
-        title={["Avid", "Media"]}
-        text={
-          <>
-            We work with a range of companies to transform their digital
-            identity, through <span>best-in-class</span> digital marketing and
-            web development.
-          </>
-        }
-        buttons={[
-          { label: "Become a client", url: "#contact" },
-          { label: "Meet the team", url: "#team" },
-        ]}
-        image={{
-          url: "/images/hero-image.jpg",
-          alt: "Projects completed by Avid Media. Including Footasylum, Primo Sports, Heat and Slice Golf Balls.",
-        }}
-      />
+      <Hero {...homepageHeroProps} />
     </>
   );
 }
---
+++ json/homepage.tsx
@@ -0,0 +1,22 @@
+import { HeroProps } from "@/components/hero/hero.type";
+import { CommonLink } from "@/types/common.type";
+
+export const primaryCta: CommonLink = {
+  label: "Become a client",
+  url: "#contact",
+};
+
+export const homepageHeroProps: HeroProps = {
+  title: ["Avid", "Media"],
+  text: (
+    <>
+      We work with a range of companies to transform their digital identity,
+      through <span>best-in-class</span> digital marketing and web development.
+    </>
+  ),
+  buttons: [primaryCta, { label: "Meet the team", url: "#team" }],
+  image: {
+    url: "/images/hero-image.jpg",
+    alt: "Projects completed by Avid Media. Including Footasylum, Primo Sports, Heat and Slice Golf Balls.",
+  },
+};
