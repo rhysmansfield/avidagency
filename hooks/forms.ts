85ce261f9e846b484cde59aa46f505a02b121683
Rhys Mansfield <rhys@avidagency.co.uk>
2025-02-26 12:38:56 +0000

    Explicit type

--- hooks/forms.ts
+++ hooks/forms.ts
@@ -1,8 +1,12 @@
 import { useCustomForm } from '@/hooks/use-custom-form';
+import { UseCustomFormResult } from '@/hooks/use-custom-form.type';
 
 import { ExampleRequest, ExampleResponse } from './forms.type';
 
-export const useExampleForm = () =>
+export const useExampleForm = (): UseCustomFormResult<
+  ExampleRequest,
+  ExampleResponse
+> =>
   useCustomForm<ExampleRequest, ExampleResponse>({
     queryKey: 'Example',
     url: '/api/example',
