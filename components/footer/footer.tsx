6be489b18812ce3e87a5ac636fe14e524f7aa1e9
Rhys Mansfield <rhys@avidagency.co.uk>
2025-02-27 15:06:37 +0000

    Add form elements

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import Link from 'next/link';
+
 import { useNewsletterForm } from '@/hooks/forms';
 
 import { Icon } from '@/components/icon/icon';
 
 import { classList } from '@/utils/class-list';
+
+import { URLs } from '@/data/urls';
 
 import { FooterProps } from './footer.type';
 
@@ -31,7 +35,7 @@ export const Footer = ({ formHeading }: FooterProps) => {
         <div className={styles['col--left']}>
           <Icon name="logo" className={styles.logo} />
           <p className={styles['form__heading']}>{formHeading}</p>
-          <form ref={ref} onSubmit={onSubmit}>
+          <form ref={ref} onSubmit={onSubmit} className={styles.form}>
             <Button
               theme="night"
               appearance="button"
@@ -41,6 +45,13 @@ export const Footer = ({ formHeading }: FooterProps) => {
               Subscribe
             </Button>
           </form>
+          <p className={styles['form__disclaimer']}>
+            By subscribing you agree to with our{' '}
+            <Link href={URLs.PRIVACY_POLICY} target="_blank">
+              privacy policy
+            </Link>{' '}
+            and provide consent to receive updates from us.
+          </p>
         </div>
 
         <div className={styles['col--right']}>Right</div>
