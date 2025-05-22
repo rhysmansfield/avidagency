436e76761bf8cdb762347e924d39f756fddb9165
Rhys Mansfield <rhys@avidagency.co.uk>
2025-05-22 14:06:18 +0100

    feat: enhance ContactForm with link to book a call and update styling for better layout

--- components/contact-form/contact-form.tsx
+++ components/contact-form/contact-form.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import Link from 'next/link';
+
 import { useContactForm } from '@/hooks/forms';
 
 import { ContactFormFields } from '@/components/contact-form-fields/contact-form-fields';
 import { ContactFormProps } from '@/components/contact-form/contact-form.type';
+import { Icon } from '@/components/icon/icon';
 
 import { classList } from '@/utils/class-list';
 
@@ -17,6 +20,11 @@ export const ContactForm = ({ theme, title, text }: ContactFormProps) => {
       <div className={styles.content}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.text}>{text}</p>
+
+        <Link href="/" className={classList('underline__wrapper', styles.link)}>
+          <span className="underline__text">Book a call</span>
+          <Icon name="arrow-right" />
+        </Link>
       </div>
       <div ref={ref} className={styles['form__wrapper']}>
         <ContactFormFields theme={theme} {...fieldProps} />
